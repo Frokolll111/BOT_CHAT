9932b76ac456e09330e818d0d7f88405e0dc1ae0
Frokolll111 <buther1433@gmail.com>
2023-10-01 21:45:10 +0700

    Исправил

--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -3,5 +3,9 @@ package pro.sky.telegrambot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.sky.telegrambot.model.NotificationTask;
 
-public interface NotificationTaskRepository extends JpaRepository<NotificationTask,Long> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask,Long>{
+    List<NotificationTask> findAllByExecDateLessThan(LocalDateTime timePoint);
 }
